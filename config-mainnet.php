5cb0d07d812eb8d3761af2922f8c5c162f923943
Adarsha Jha <adarshajha@live.com>
2018-03-06 17:34:27 +0530

    extra file removed

--- config-mainnet.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-   'rk_user' => 'rkrpc',
-   'rk_pass' => 'GnCeLzpeZAWJ2RkGNJusYuSUiZdDuAuEkhZQTw31pATe',
-   "rk_host" => 'http://35.172.1.247:7344',
-   "rk_port" => '7344',
-   "chain" => 'recordskeeper'
-);
