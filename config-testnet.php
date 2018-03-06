6081cffa9de80039a9919c7726fcb33c8b65e58c
Adarsha Jha <adarshajha@live.com>
2018-03-06 17:34:42 +0530

    unwanted file removal

--- config-testnet.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-  'rk_user' => 'rkrpc',
-  'rk_pass' => '3W4hprQLwshx7dijhSBY3f2ofN4cdjbheCeR1VF7zxcp',
-  "rk_host" => 'http://35.170.155.89:8378',
-  "rk_port" => '8378',
-  "chain" => 'recordskeeper-test'
-);
