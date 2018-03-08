6b631fc244c3d0d0b98748a1b80f7b823c28702f
Adarsha Jha <adarshajha@live.com>
2018-03-08 14:27:36 +0530

    optional data validation

--- createrawsendfrom.php
+++ createrawsendfrom.php
@@ -12,7 +12,28 @@ $key = $_POST['key'];
 $val = $_POST['val'];
 $to = $_POST['to'];
 $amount =$_POST['amount'];
-curl_setopt_array($curl, array(
+if($key == "" && $val == ""){
+  curl_setopt_array($curl, array(
+   CURLOPT_PORT => $config['rk_port'],
+  CURLOPT_URL => $config['rk_host'],
+  CURLOPT_USERPWD => $config['rk_user'].":".$config['rk_pass'],
+  CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\"method\":\"createrawsendfrom\",\"params\":[\"$addr\",{\"$to\":$amount},[]],\"id\":\"curltext\",\"chain_name\":\"$chain\"}",
+  CURLOPT_HTTPHEADER => array(
+    "Cache-Control: no-cache",
+    "Content-Type: application/json",
+    "Postman-Token: a04df090-2ea9-9b78-e373-c71b54d8b41b"
+  ),
+));
+}
+else{
+  curl_setopt_array($curl, array(
    CURLOPT_PORT => $config['rk_port'],
   CURLOPT_URL => $config['rk_host'],
   CURLOPT_USERPWD => $config['rk_user'].":".$config['rk_pass'],
@@ -30,6 +51,7 @@ curl_setopt_array($curl, array(
     "Postman-Token: a04df090-2ea9-9b78-e373-c71b54d8b41b"
   ),
 ));
+}
 $response = curl_exec($curl);
 $err = curl_error($curl);
 curl_close($curl);
--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -19,6 +19,10 @@ $("#sendpopup").click(function(){
 
 var  pubaddr = localStorage.getItem("pubaddr");
 
+var keyData = document.getElementById("keydata");
+
+var hexData = document.getElementById("hexdata");
+
  var inputVal = document.getElementById("sendRecipientaddress");
 
  var inputVal1 = document.getElementById("sendBTC");
@@ -89,9 +93,89 @@ function checkBoth(){
 		 $("#sendpopup").removeAttr('data-toggle');
 		
 		}
+		if (inputVal.value != "" && inputVal1.value != "" &&  keyData.value != ""  ) {
+		 $('#hexdata').css('border', '1px solid red');
+		 // $('#sendBTC').css('border', '1px solid red');
+		 $("#sendpopup").removeAttr('data-toggle');
 		
+		}
 
-		else{
+		if (inputVal.value != "" && inputVal1.value != "" &&  keyData.value != ""  &&  hexData.value != "") {
+			
+				$('#hexdata').css('border', '1px solid green');
+				$('#keydata').css('border', '1px solid green');
+
+				var netw = net;
+
+                var senderAddress = $('#sendRecipientaddress').val();
+
+                   $('#sendpopup').removeAttr('data-toggle');
+
+                    $.ajax({
+                     type: "POST",
+                     url: 'validateaddress.php',
+                     data:{net:netw, senderAddress: senderAddress},
+                     success:function(Response) {
+                         var x = Response;
+                         x = JSON.parse(x);
+
+                          //  x = x.result;
+
+
+                        CONSOLE_DEBUG && console.log('validate address result :', x);
+                       
+                        var  validateaddr =  x.result['isvalid'];
+                         
+                         CONSOLE_DEBUG && console.log('validate address result :', validateaddr);
+                         
+                          if( validateaddr == false){
+
+                             CONSOLE_DEBUG && console.log('You Entered an invalid Recipient Address');
+
+                             $('#myModal2').modal('hide');
+
+                              // $('#modalshowaddr').text('You Entered an invalid Recipient Address');
+                              // $('#modalshowaddr').css("color", "#c13434");
+                              // $('#sendUSD').css("display", "none");
+                              // $('#sendt').css("display", "none");
+                              //  $('h4.modal-title').css("color", "#c13434");
+                              // $('h4.modal-title').text('ERROR !');
+
+                              // $('#sendpopup').attr("data-target", "myModal3");
+
+                           $('#myModal1').modal('show');
+
+
+
+                          }else{
+
+                          	  $('#myModal1').modal('hide');
+
+                          	CONSOLE_DEBUG && console.log('You Entered valid Recipient Address');
+                          	 // $('h4.modal-title').text('Authorize Transaction ');
+                          	 // $('h4.modal-title').css("color", "#3f4453");
+                          	 // $('#modalshowaddr').css("color", "#3f4453");
+                          	 // $('#sendt').css("display", "block");
+                          	 //  $('#sendUSD').css("display", "block");
+                          	 // $('#modalshowaddr').text('Public Address : '+ pubaddr);
+                          	 // document.getElementById('modalshowaddress').innerHTML = 'Public Address : '+ pubaddr;
+
+                          	 $('#myModal2').modal('show');
+
+                          }
+                         
+                       
+                       
+
+                               
+                       }
+                   });
+
+			 $("#sendpopup").attr("data-toggle", "modal");
+		
+		}
+		if (inputVal.value != "" && inputVal1.value != "" &&  keyData.value == ""  &&  hexData.value == "") {
+				
 
 
 				var netw = net;
@@ -161,6 +245,10 @@ function checkBoth(){
                    });
 
 			 $("#sendpopup").attr("data-toggle", "modal");
+		
 		}
+		
+
+		
 
 	}	
