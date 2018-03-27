8f66a70ba8d9eeedb53e8d1bcfa0b4505922dec3
Adarsha Jha <adarshajha@live.com>
2018-03-27 12:17:27 +0530

    airdrop and minor link updated

--- index.php
+++ index.php
@@ -123,6 +123,8 @@
 <li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
 <li><a href="http://stats.recordskeeper.co/" target="_blank">Stats</a></li>
 <li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
+<li><a href="http://miner.recordskeeper.co/" target="_blank">Miner</a></li>
+<li><a href="http://aidrop.recordskeeper.co/" target="_blank">Airdrop</a></li>
 </ul>
 </footer>
 </div>
