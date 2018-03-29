acc13d8f14a8fa7c258b6151437ae8848558d46a
adarsha jha <adarshajha@live.com>
2018-03-29 12:41:39 +0530

    footer updated with new links

--- home.php
+++ home.php
@@ -314,16 +314,17 @@
 
 <footer id="footer">
 <ul>
-<li>© 2016-2018<span class="date">  RecordsKeeper. All rights Reserved</span></li>
-<li><a href="./" target="_blank">Terms</a></li>
-<li><a href="./" target="_blank">Privacy Policy</a></li>
-<li><a href="http://explorer.recordskeeper.co/" target="_blank">Mainnet Explorer</a></li>
-<li><a href="http://test-explorer.recordskeeper.co/" target="_blank">Testnet Explorer</a></li>
-<li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
-<li><a href="http://stats.recordskeeper.co/" target="_blank">Stats</a></li>
-<li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
-<li><a href="http://airdrop.recordskeeper.co/" target="_blank">Airdrop</a></li>
-<li><a href="http://miner.recordskeeper.co/" target="_blank">Miner</a></li>
+		<li>&copy; RecordsKeeper @<span class="date">2016-2018. All rights reserved</span></li>
+		<li><a href="./" target="_blank">Terms</a></li>
+		<li><a href="./" target="_blank">Privacy Policy</a></li>
+		<li><a href="http://explorer.recordskeeper.co/" target="_blank">Mainnet Explorer</a></li>
+		<li><a href="http://test-explorer.recordskeeper.co/" target="_blank">Testnet Explorer</a></li>
+		<li><a href="http://wallet.recordskeeper.co/" target="_blank">Wallet</a></li>
+		<li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
+		<li><a href="http://stats.recordskeeper.co/" target="_blank">Blockchain Statistics</a></li>
+		<li><a href="http://miner.recordskeeper.co/" target="_blank">Miner Statistics</a></li>
+		<li><a href="http://airdrop.recordskeeper.co/" target="_blank">Airdrop</a></li>
+		<li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
 
 </ul>
 </footer>
