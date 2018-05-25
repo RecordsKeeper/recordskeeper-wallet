447b160b91553c10933380c5dd8579449813b11d
adarsha jha <adarshajha@live.com>
2018-05-25 16:17:04 +0530

    amount low excepetion added

--- javascript/signer.js
+++ javascript/signer.js
@@ -36,110 +36,127 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
 function signMultisigTransaction(){
 
-	var multisigtransactionHex = getUrlParameter('multisig');
-	CONSOLE_DEBUG && console.log("multisigHex", multisigtransactionHex);
 
-	var redeemScript = getUrlParameter('redeemScript');
-	CONSOLE_DEBUG && console.log("redeemScript", redeemScript);
+  var getUrlParameter = function getUrlParameter(sParam) {
+         var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+         sURLVariables = sPageURL.split('&'),
+         sParameterName,
+         i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
 
-	var decodeMultisigVinTxid = getUrlParameter('txid');
-	CONSOLE_DEBUG && console.log("txid", decodeMultisigVinTxid);
-
-	var getRawTransactionResp = getUrlParameter('getRawTransactionResp');
-	CONSOLE_DEBUG && console.log("getRawTransactionResp", getRawTransactionResp);
-
-	var decodeMultisigVout = getUrlParameter('vout');
-	CONSOLE_DEBUG && console.log("vout", decodeMultisigVout);
-
-    jQuery.ajax({
-            type: "POST",
-            url: 'signmultitransaction.php',
-           
-            data: {
-                 net: net,   
-                 multisigtransactionHex : multisigtransactionHex,
-                 redeemScript : redeemScript,
-                 decodeMultisigVinTxid : decodeMultisigVinTxid,
-                 getRawTransactionResp : getRawTransactionResp,
-                 decodeMultisigVout : decodeMultisigVout,
-                 sendmultisigvalue : sendmultisigvalue
-                
-            },
-
-            success: function(Response) {
-               
-               var signmultiTransactionRes = Response ; 
-
-               signmultiTransactionRes = JSON.parse(signmultiTransactionRes);
-
-                CONSOLE_DEBUG && console.log("signmultiTransactionRes", signmultiTransactionRes);
-
-                if ( signmultiTransactionRes.result == null ){
-
-                	swal({
-                	icon: "error",
-                    title: 'Incorrect Private Key',
-                    html: '<p></p>',
-                    type: 'error',
-                    confirmButtonClass: "btn-danger",
-                    confirmButtonText: "OK!",
-                    timer: 15000
-                });
-
-                }
-
-               else{
-
-               				signmultiTransactionComplete = signmultiTransactionRes.result.complete;
-               				CONSOLE_DEBUG && console.log("signmultiTransactionComplete", signmultiTransactionComplete);
-               				 multisigsendhex = signmultiTransactionRes.result.hex;
-							
-
-				               if ( signmultiTransactionComplete == false){
-
-				                   signmultiTransactionHex = signmultiTransactionRes.result.hex;
-
-				                   CONSOLE_DEBUG && console.log("signmultiTransactionRes", signmultiTransactionHex);
-
-				                   
-
-				                    var URLBase = "http://localhost:8888/wallet/recordskeeper-wallet/signer.php?multisig=";
-				                    var TrailingFixedData = signmultiTransactionHex;
-
-				                    finalURL = URLBase +  TrailingFixedData;
-				                    CONSOLE_DEBUG && console.log("finalURL", finalURL);
-
-				                   jQuery(".signtransUrl").css("display", "block");
-				                   jQuery(".signurl").text(finalURL);
-				                   jQuery(".asignhref").attr("href", finalURL);
-
-	            				}
-				               else {
-
-				                 
-                           // sendmultisig();
-							            
-                           sendmultisig();
-
-							             if(net == "TestNetwork" ){
-
-							             	transactionUrl = "https://test-explorer.recordskeeper.co/RecordsKeeper%20Testnet/tx/" ;
-							             }
-							             else{
-
-							             	transactionUrl = "https://explorer.recordskeeper.co/RecordsKeeper%20Mainnet/tx/" ;
-							             }
-
-							             
-							            
-
-
-                      
-				               }
-               }
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
             }
+        }
+};
 
-     });
+    	var multisigtransactionHex = getUrlParameter('multisig');
+    	CONSOLE_DEBUG && console.log("multisigHex", multisigtransactionHex);
+
+    	var redeemScript = getUrlParameter('redeemScript');
+    	CONSOLE_DEBUG && console.log("redeemScript", redeemScript);
+
+    	var decodeMultisigVinTxid = getUrlParameter('txid');
+    	CONSOLE_DEBUG && console.log("txid", decodeMultisigVinTxid);
+
+    	var getRawTransactionResp = getUrlParameter('getRawTransactionResp');
+    	CONSOLE_DEBUG && console.log("getRawTransactionResp", getRawTransactionResp);
+
+    	var decodeMultisigVout = getUrlParameter('vout');
+    	CONSOLE_DEBUG && console.log("vout", decodeMultisigVout);
+
+      jQuery.ajax({
+              type: "POST",
+              url: 'signmultitransaction.php',
+             
+              data: {
+                   net: net,   
+                   multisigtransactionHex : multisigtransactionHex,
+                   redeemScript : redeemScript,
+                   decodeMultisigVinTxid : decodeMultisigVinTxid,
+                   getRawTransactionResp : getRawTransactionResp,
+                   decodeMultisigVout : decodeMultisigVout,
+                   sendmultisigvalue : sendmultisigvalue
+                  
+              },
+
+              success: function(Response) {
+                 
+                 var signmultiTransactionRes = Response ; 
+
+                 signmultiTransactionRes = JSON.parse(signmultiTransactionRes);
+
+                  CONSOLE_DEBUG && console.log("signmultiTransactionRes", signmultiTransactionRes);
+
+                  if ( signmultiTransactionRes.result == null ){
+
+                  	swal({
+                  	icon: "error",
+                      title: 'Incorrect Private Key',
+                      html: '<p></p>',
+                      type: 'error',
+                      confirmButtonClass: "btn-danger",
+                      confirmButtonText: "OK!",
+                      timer: 15000
+                  });
+
+                  }
+
+                 else{
+
+                 				signmultiTransactionComplete = signmultiTransactionRes.result.complete;
+                 				CONSOLE_DEBUG && console.log("signmultiTransactionComplete", signmultiTransactionComplete);
+                 				 multisigsendhex = signmultiTransactionRes.result.hex;
+  							
+
+  				               if ( signmultiTransactionComplete == false){
+
+  				                   signmultiTransactionHex = signmultiTransactionRes.result.hex;
+
+  				                   CONSOLE_DEBUG && console.log("signmultiTransactionRes", signmultiTransactionHex);
+
+  				                   
+
+  				                    var URLBase = "http://localhost:8888/wallet/recordskeeper-wallet/signer.php?multisig=";
+  				                    var TrailingFixedData = signmultiTransactionHex;
+
+  				                    finalURL = URLBase +  TrailingFixedData + "&redeemScript="+redeemScript+"&txid="+decodeMultisigVinTxid+"&vout="+decodeMultisigVout+"&getRawTransactionResp="+getRawTransactionResp;
+  				                    CONSOLE_DEBUG && console.log("finalURL", finalURL);
+
+  				                   jQuery(".signtransUrl").css("display", "block");
+  				                   jQuery(".signurl").text(finalURL);
+  				                   jQuery(".asignhref").attr("href", finalURL);
+                             
+
+  	            				}
+
+  				               else {
+
+  				                 
+                             // sendmultisig();
+  							            
+                             sendmultisig();
+
+  							             if(net == "TestNetwork" ){
+
+  							             	transactionUrl = "https://test-explorer.recordskeeper.co/RecordsKeeper%20Testnet/tx/" ;
+  							             }
+  							             else{
+
+  							             	transactionUrl = "https://explorer.recordskeeper.co/RecordsKeeper%20Mainnet/tx/" ;
+  							             }
+
+  							             
+  							            
+
+
+                        
+  				               }
+                 }
+              }
+
+       });
 
 }
 
@@ -160,16 +177,16 @@ function sendmultisig(){
                
                var multiSigResponse = Response ;
 
-               multiSigResponse = JSON.parse(multiSigResponse);
-               multiSigResponse = multiSigResponse.result ;
+               multiSigResp = JSON.parse(multiSigResponse);
+               multiSigResponse = multiSigResp.result ;
                CONSOLE_DEBUG && console.log("sendmultisig Response : ", multiSigResponse);
                transactionID = multiSigResponse ;
 
-              if( multiSigResponse == null  ){
+              if( multiSigResponse == null && multiSigResp.error['code'] == -26  ){
 
                		swal({
 	                	icon: "error",
-	                    title: 'Transaction already in Blockchain',
+	                    title: 'Amount is too low for multisig transaction',
 	                    html: '<p></p>',
 	                    type: 'error',
 	                    confirmButtonClass: "btn-danger",
@@ -178,7 +195,22 @@ function sendmultisig(){
                		 });
 
                }
-               else{
+
+               else if( multiSigResponse == null ){
+
+                  swal({
+                    icon: "error",
+                      title: 'Transaction already in Blockchain',
+                      html: '<p></p>',
+                      type: 'error',
+                      confirmButtonClass: "btn-danger",
+                      confirmButtonText: "OK!",
+                      timer: 15000
+                   });
+
+               }
+
+               else {
                  swal({
                               title: 'Transaction Successful !',
                               html: '<a href="' + transactionUrl + transactionID +'" target="_blank"> <b>Check Transaction status here:</b><br>'+ transactionID +'</a>',
